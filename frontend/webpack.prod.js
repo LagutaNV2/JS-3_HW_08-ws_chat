d4b8283e80ffa7624ece90f6f6ef6513b3e05361
Natali LaGuta <n.v.laguta2023@gmail.com>
2025-03-04 13:44:06 +0100

    configs

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'production',
